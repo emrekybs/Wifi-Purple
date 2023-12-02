54cdf81f89e05eceb6d12bb21654c25ddb3ea8d5
EmreKybs <emrekoybasi@protonmail.com>
2023-12-02 21:33:04 +0300

    install.sh

--- install.sh
+++ install.sh
@@ -5,14 +5,13 @@ sudo apt-get install -y aircrack-ng
 sudo apt-get install -y bully
 sudo apt-get install -y wifite
 clear
-# Kurulum başarılı mesajı
+
 if [ $? -eq 0 ]; then
     echo -e "\033[1;35mInstallation successful!\033[0m"
     
-    # Bekleme
     sleep 2
     
-    # Temizle ve wifi-purple.py çalıştır
+
     clear
     sudo python3 wifi-purple.py
 else
